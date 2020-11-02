2407cdf4e37aa8818d123ea05d6530ffa7ddafba
po45ke95 <po45ke95@gmail.com>
2020-11-02 17:39:58 +0800

    2020-11-02-3

--- plots/plot.py
+++ plots/plot.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import numpy as np
+import seaborn as sns
 data = pd.read_csv('newcode02.csv',index_col='Item')
 #print(data.head(10))
 gender = data.groupby('PRO1Gender')
-city = data.groupby('PRO2City')
-#print(type(gender))
-#print(gender.size())
-#print(city.size())
-gender.get_group(1).head
+plt.ylabel('People')
+gender.size().plot.bar()
+plt.ylim(520,560)
+plt.grid(True)
+
+plt.show()
