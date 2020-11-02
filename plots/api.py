9d2181f1afb6bafceebab80d86c5f1abc6d3a8fa
po45ke95 <po45ke95@gmail.com>
2020-11-03 04:06:39 +0800

    2020-11-02-4

---
+++ plots/api.py
@@ -0,0 +1,25 @@
+from flask import Flask,url_for
+app = Flask(__name__)
+
+#@app.route('/')
+#def index():
+#    return 'hello man'
+
+#@app.route('/user/<username>')
+#def username(username):
+#    return 'i am ' + username
+
+#@app.route('/age/<int:age>')
+#def user(age):
+#    return 'i am ' + str(age) + ' years old'
+@app.route('/a')
+def url_for_a():
+    return 'here is a'
+
+@app.route('/b')
+def b():
+    return url_for('url_for_a')    
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
